507abd2bf45300030c40548420257e476141dd52
ArtiOJ <jamespodom@gmail.com>
2026-01-11 05:52:20 +0900

    Progress save

--- src/main/java/dev/artifabiran/sunlight/Sunlight.java
+++ src/main/java/dev/artifabiran/sunlight/Sunlight.java
@@ -1,20 +1,24 @@
 package dev.artifabiran.sunlight;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Sunlight extends JavaPlugin {
 
     SunlightData sunlightData;
+    TimeController timeController;
+    YamlConfiguration config;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
         sunlightData = new SunlightData(this);
-//        sunlightData.getConfig().get("tickCounter");
-//        sunlightData.getConfig().set("tickCounter", 0);
+        config = sunlightData.getConfig();
+        timeController = new TimeController(config.getLong("tickCounter"));
     }
 
     @Override
     public void onDisable() {
-
+        config.set("tickCounter", timeController.getTickCounter());
     }
 }
--- src/main/java/dev/artifabiran/sunlight/TimeController.java
+++ src/main/java/dev/artifabiran/sunlight/TimeController.java
@@ -3,8 +3,19 @@ package dev.artifabiran.sunlight;
 public class TimeController {
     private long tickCounter;
 
+    public TimeController(long tickCounter) {
+        this.tickCounter = tickCounter;
+    }
     public long getTickCounter() {
         return tickCounter;
+    }
+
+    public double calculateDayMultiplier() {
+        return 0.0;
+    }
+
+    public void setTickCounter(long tickCounter) {
+        this.tickCounter = tickCounter;
     }
 }
 
