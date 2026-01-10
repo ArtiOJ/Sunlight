e70d3185cad2e05f09a3760daf3cfa72a6aa751c
ArtiOJ <jamespodom@gmail.com>
2026-01-11 04:59:43 +0900

    Added Config

---
+++ src/main/java/dev/artifabiran/sunlight/SunlightData.java
@@ -0,0 +1,36 @@
+package dev.artifabrian.sunlight;
+
+import dev.artifabiran.sunlight.Sunlight;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class SunlightData {
+    private Sunlight plugin;
+    private File file;
+    private YamlConfiguration config;
+
+    public SunlightData(Sunlight plugin){
+        this.plugin = plugin;
+        load();
+    }
+
+    private void load(){
+        file = new File(plugin.getDataFolder(), "config.yml");
+
+        plugin.saveResource("config.yml", false);
+
+        config = new YamlConfiguration();
+        config.options().parseComments(false);
+
+        try {
+            config.load(file);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public YamlConfiguration getConfig(){
+        return config;
+    }
+}
