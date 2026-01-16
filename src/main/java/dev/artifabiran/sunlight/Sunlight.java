475f9283f991ab2474c3a0639c58f93a7d3b0318
artifabrian <jamespodom@gmail.com>
2026-01-16 18:23:11 -0500

    Automatic day/night cycle disableing.

--- src/main/java/dev/artifabiran/sunlight/Sunlight.java
+++ src/main/java/dev/artifabiran/sunlight/Sunlight.java
@@ -2,6 +2,9 @@ package dev.artifabiran.sunlight;
 
 import dev.artifabiran.sunlight.commands.SetDaytimeCommand;
 import dev.artifabiran.sunlight.commands.SetNighttimeCommand;
+import dev.artifabiran.sunlight.util.Colorize;
+import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,6 +19,11 @@ public final class Sunlight extends JavaPlugin {
         // Plugin startup logic
         sunlightData = new SunlightData(this);
         config = sunlightData.getConfig();
+        if (Boolean.TRUE.equals(Bukkit.getWorld("world").getGameRuleValue(GameRule.DO_DAYLIGHT_CYCLE))) {
+            Bukkit.getLogger().warning(Colorize.format("[Sunlight] &cGamerule doDaylightCycle has been automatically disabled to allow Sunlight plugin to handle day/night cycle."));
+            Bukkit.getWorld("world").setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+        }
+
         timeController = new TimeController(this, config.getLong("tickCounter"), config.getInt("daytimeMultiplier"), config.getInt("nighttimeMultiplier"));
 
         new SetDaytimeCommand(this, timeController, sunlightData);
